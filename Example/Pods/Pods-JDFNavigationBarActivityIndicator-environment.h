f919168a896c9632d9b63c04d79174e36774f649
Joe Fryer <joe.d.fryer@gmail.com>
2014-07-29 23:54:52 +0100

    Project setup.

---
+++ Example/Pods/Pods-JDFNavigationBarActivityIndicator-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// JDFNavigationBarActivityIndicator
+#define COCOAPODS_POD_AVAILABLE_JDFNavigationBarActivityIndicator
+#define COCOAPODS_VERSION_MAJOR_JDFNavigationBarActivityIndicator 0
+#define COCOAPODS_VERSION_MINOR_JDFNavigationBarActivityIndicator 1
+#define COCOAPODS_VERSION_PATCH_JDFNavigationBarActivityIndicator 0
+
